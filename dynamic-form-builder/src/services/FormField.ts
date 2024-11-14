9d1367727fd7b29925fadd5603cc18dfbcc0a221
Marko Andonov <marko.20112000@gmail.com>
2024-11-14 01:11:45 +0100

    format code

--- dynamic-form-builder/src/services/FormField.ts
+++ dynamic-form-builder/src/services/FormField.ts
@@ -2,11 +2,15 @@ export class FormField {
   public id: string
   public label: string
   public validationRules: {
-    [key: string]: string | boolean | number | undefined; // Explicitly allow undefined
-  };
+    [key: string]: string | boolean | number | undefined // Explicitly allow undefined
+  }
   public value: string | number | boolean
 
-  constructor(id: string, label: string, validationRules: { [key: string]: string | boolean | number | undefined } = {}) {
+  constructor(
+    id: string,
+    label: string,
+    validationRules: { [key: string]: string | boolean | number | undefined } = {},
+  ) {
     this.id = id
     this.label = label
     this.validationRules = validationRules
@@ -17,7 +21,11 @@ export class FormField {
 export class TextField extends FormField {
   public type: string
 
-  constructor(id: string, label: string, validationRules: { [key: string]: string | boolean | number | undefined }) {
+  constructor(
+    id: string,
+    label: string,
+    validationRules: { [key: string]: string | boolean | number | undefined },
+  ) {
     super(id, label, validationRules)
     this.type = 'text'
     this.value = ''
@@ -27,7 +35,11 @@ export class TextField extends FormField {
 export class NumberField extends FormField {
   public type: string
 
-  constructor(id: string, label: string, validationRules: { [key: string]: string | boolean | number | undefined }) {
+  constructor(
+    id: string,
+    label: string,
+    validationRules: { [key: string]: string | boolean | number | undefined },
+  ) {
     super(id, label, validationRules)
     this.type = 'number'
     this.value = 0
@@ -37,7 +49,11 @@ export class NumberField extends FormField {
 export class CheckboxField extends FormField {
   public type: string
 
-  constructor(id: string, label: string, validationRules: { [key: string]: string | boolean | number | undefined }) {
+  constructor(
+    id: string,
+    label: string,
+    validationRules: { [key: string]: string | boolean | number | undefined },
+  ) {
     super(id, label, validationRules)
     this.type = 'checkbox'
     this.value = false
@@ -48,7 +64,12 @@ export class DropdownField extends FormField {
   public type: string
   public options: string[]
 
-  constructor(id: string, label: string, options: string[] = [], validationRules: { [key: string]: string | boolean | number | undefined }) {
+  constructor(
+    id: string,
+    label: string,
+    options: string[] = [],
+    validationRules: { [key: string]: string | boolean | number | undefined },
+  ) {
     super(id, label, validationRules)
     this.type = 'dropdown'
     this.options = options
