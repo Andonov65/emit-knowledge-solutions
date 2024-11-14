b8e20119cefc81e42fca9cc432384fef8200848b
Marko Andonov <marko.20112000@gmail.com>
2024-11-14 02:04:53 +0100

    init second project

---
+++ hacker-news/src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+ <div class="body-class">
+   <header>
+
+     <div>
+       Yeyy
+
+       <nav>
+         <RouterLink to="/">Home</RouterLink>
+         <RouterLink to="/about">About</RouterLink>
+       </nav>
+     </div>
+   </header>
+
+   <RouterView />
+ </div>
+</template>
+
+<style scoped>
+.body-class {
+  font-family: Arial, sans-serif;
+  padding: 20px;
+}
+</style>
