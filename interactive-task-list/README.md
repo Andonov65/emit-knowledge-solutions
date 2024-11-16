18baa8eb23ece58f577ac5f84fd82d0b7b2e5ec8
Marko Andonov <marko.20112000@gmail.com>
2024-11-16 12:43:24 +0100

    create interactive-task-list

--- interactive-task-list/README.md
+++ interactive-task-list/README.md
@@ -1,4 +1,4 @@
-# hacker-news
+# interactive-task-list
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- interactive-task-list/src/App.vue
+++ interactive-task-list/src/App.vue
@@ -1,28 +1,47 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
- <div class="body-class">
-   <header>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-     <div>
-       Yeyy
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
 
-       <nav>
-         <RouterLink to="/">Home</RouterLink>
-         <RouterLink to="/about">About</RouterLink>
-       </nav>
-     </div>
-   </header>
-
-   <RouterView />
- </div>
+  <main>
+    <TheWelcome />
+  </main>
 </template>
 
 <style scoped>
-.body-class {
-  font-family: Arial, sans-serif;
-  padding: 20px;
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
 }
 </style>
